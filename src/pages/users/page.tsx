2c543baee6521707bb49f11eb5132b7ecf706c83
ParomovEvg <evgeny.paromov@gmail.com>
2024-12-21 21:16:08 +0300

    feat: add fetch users

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,64 +1,83 @@
-import { useState } from "react";
+import { Suspense, use, useState } from "react";
+import { createUser, fetchUsers } from "../../shared/api";
 
 type User = {
   id: string;
   email: string;
 };
 
+const defaultUsersPromise = fetchUsers();
+
 export function UsersPage() {
-  const [users, setUsers] = useState<User[]>([]);
-
-  const [email, setEmail] = useState("");
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setUsers([...users, { id: crypto.randomUUID(), email }]);
-    setEmail("");
-  };
-
-  const handleDelete = (id: string) => {
-    setUsers((lastUsers) => lastUsers.filter((user) => user.id !== id));
+  const [usersPromise, setUsersPromise] = useState(defaultUsersPromise);
+  const refetchUsers = () => {
+    setUsersPromise(fetchUsers());
   };
 
   return (
     <main className="container mx-auto p-4 pt-10 flex flex-col gap-4">
       <h1 className="text-3xl font-bold underline">Users</h1>
-      <section>
-        <form className="flex gap-2" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            className="border p-2 rounded"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Add
-          </button>
-        </form>
-      </section>
-      <section>
-        <div className="flex flex-col">
-          {users.map((user) => (
-            <div
-              className="border p-2 m-2 rounded bg-gray-100 flex gap-2"
-              key={user.id}
-            >
-              {user.email}
-
-              <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
-                type="button"
-                onClick={() => handleDelete(user.id)}
-              >
-                Delete
-              </button>
-            </div>
-          ))}
-        </div>
-      </section>
+      <CreateUserForm refetchUsers={refetchUsers} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <UsersList usersPromise={usersPromise} />
+      </Suspense>
     </main>
+  );
+}
+
+export function CreateUserForm({ refetchUsers }: { refetchUsers: () => void }) {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await createUser({
+      email,
+      id: crypto.randomUUID(),
+    });
+    refetchUsers();
+    setEmail("");
+  };
+
+  return (
+    <form className="flex gap-2" onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="border p-2 rounded"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="submit"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export function UsersList({ usersPromise }: { usersPromise: Promise<User[]> }) {
+  const users = use(usersPromise);
+  return (
+    <div className="flex flex-col">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+}
+
+export function UserCard({ user }: { user: User }) {
+  return (
+    <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2">
+      {user.email}
+
+      <button
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
+        type="button"
+      >
+        Delete
+      </button>
+    </div>
   );
 }
---
+++ src/shared/api.ts
@@ -0,0 +1,26 @@
+export type User = {
+  id: string;
+  email: string;
+};
+
+export function fetchUsers() {
+  return fetch("http://localhost:3001/users").then(
+    (res) => res.json() as Promise<User[]>
+  );
+}
+
+export function createUser(user: User) {
+  return fetch("http://localhost:3001/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  }).then((res) => res.json());
+}
+
+export function deleteUser(id: string) {
+  return fetch(`http://localhost:3001/users/${id}`, {
+    method: "DELETE",
+  }).then((res) => res.json());
+}
